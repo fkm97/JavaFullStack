c0823e5d3bd5d137626ea6e0315a4d94549deb61
fkm97 <fardeenkhalid@gmail.com>
2023-05-25 18:10:47 -0400

    finished the backend API

---
+++ movies/src/main/java/dev/fkm97/movies/MovieService.java
@@ -0,0 +1,21 @@
+package dev.fkm97.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+
+}
---
+++ movies/src/main/java/dev/fkm97/movies/ReviewRepository.java
@@ -0,0 +1,12 @@
+package dev.fkm97.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+
+
+}
